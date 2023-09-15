83996d802601af2d86798c5dab49a0a88964ed35
Hernan Melia <hernanmelia@me.com>
2023-09-15 05:27:59 +0200

    Improved the home section

--- src/components/Pages/HomeContent.tsx
+++ src/components/Pages/HomeContent.tsx
@@ -2,24 +2,24 @@ import WarningMessage from "../WarningMessage";
 
 function HomeContent() {
   return (
-    <main className="px-10">
+    <main className="px-10 flex flex-col items-center gap-4">
       <h1 className="font-semibold text-2xl">Welcome to Phases App</h1>
-      <p className="text-xl">
-        👈 Select an startup in the sidebar or create a new one.
-      </p>
+      <div className="flex gap-4">
+        <p>👈</p>
+        <p className="text-xl">
+          Select an startup in the sidebar or create a new one.
+        </p>
+      </div>
       <WarningMessage>
         <strong className="block font-medium text-red-800">
-          Just a note about the app usage
+          How to use the app
         </strong>
         <p className="mt-2 text-sm text-red-700">
-          You will find the "+ Add Startup" button in the sidebar. For editing
-          the entities, you have to click the text and after editing, quitting
-          focus from the input or hitting the enter key will do the job; with
-          the escape key, you can cancel the edition. Also note that if you
-          empty the input and hit enter, the edition will be canceled. This is
-          what I could do in my free time during the weekend, It is a little bit
-          glitchy, but it is honest work. I hope you like it and thank you for
-          your time!
+          You'll spot the <strong>Add Startup</strong> button in the sidebar.
+          For editing, just click the text. To finish editing, either exit the
+          input or hit Enter. Cancel with the Escape key. If you clear the input
+          and press Enter, the edit cancels. It's a weekend project, a bit
+          buggy, but honest work. Hope you like it, and thanks for your time!
         </p>
       </WarningMessage>
     </main>
